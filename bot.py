1e1b46581c7fa835c7c0bb464e08c6aa4a24d839
Fehmi Noyan Isi <fnoyanisi@yahoo.com>
2025-03-23 00:25:02 +1300

    moved mechanics of buy/sell to broker and kept the bot simple

--- bot.py
+++ bot.py
@@ -13,7 +13,6 @@ class TradeKitBot:
         self.commission = 0
         self.db = db
         self.position = None
-        self.position_updated = False
         self.cash = 0.0
 
         # determines the % of cash that can be spent 
@@ -38,6 +37,11 @@ class TradeKitBot:
             "conservative": conservative_ratio
         }
 
+    def deposit(self, amount: float):
+        if amount < 0:
+            raise ValueError("Deposit amount must be positive.")
+        self.cash += amount
+
     def load_data(self, data):
         """Load the historic trade data. Must include OHLC and volume information."""
         if not isinstance(data, pd.DataFrame):
@@ -59,14 +63,12 @@ class TradeKitBot:
     def load_position(self):
         """Load the latest open position from the database"""
         self.position = self.db.get_last_position(self.name, self.ticker)
-        if self.position:
-            self.position_updated = True
 
     def get_position(self):
         """Return the current trade position."""
-        return self.position if self.position_updated else None
+        return self.position
 
-    def run(self, strategy: Optional[Callable[['TradeBot'], None]] = None):
+    def run(self, strategy: Optional[Callable[['TradeKitBot'], None]] = None):
         """
         Loads the latest position from the database and runs the strategy. 
         The strategy object is a function that expects exactly one argument of type 
@@ -85,51 +87,56 @@ class TradeKitBot:
         quantity = int(budget_after_commission / price)
         return max(quantity, 0)
 
-    def place_buy_order(self, position_type: str, price: float):
+    def buy(self, position_type: str, price: float, observed_date: Optional[str] = None):
         """Place a buy order."""
         quantity = self.calculate_quantity(price)
         if quantity > 0:
-            if position_type == "LONG":
-                self.position = TradePosition(
+            if not self.position:
+                self.position = TradeKitPosition(
                     bot_name=self.name,
                     ticker=self.ticker,
-                    trade_type=position_type,
+                    position_type=position_type,
                     quantity=quantity,
-                    open_order_price=price,
+                    observed_date=observed_date,
+                    entry_submit_price=price,
                     action="BUY"
                 )
-                self.position.place_open_order(price)
-                self.position_updated = True
-                self.db.open_order(self.position)
-            elif position_type == "SHORT":
-                if self.position:
-                    self.position.place_close_order(price)
-                    self.position_updated = True
-                    self.db.close_trade(self.position)
+            # the order hasn't been executed yet
+            self.submit_order(position_type=position_type, price=price)
+            return quantity
         else:
             print("Not enough cash to place a trade: " + str(self.cash))
+            return -1
 
-    def place_sell_order(self, position_type: str, quantity: int, price: float):
+    def sell(self, position_type:str, quantity: int, price: float, observed_date: Optional[str] = None):
         """Place a sell order."""
-        if position_type == "LONG":
-            if self.position:
-                self.position.place_close_order(price)
-                self.position_updated = True
-                self.db.close_trade(self.position)
-        elif position_type == "SHORT":
-            self.position = TradePosition(
+        if not self.position:
+            self.position = TradeKitPosition(
                 bot_name=self.name,
                 ticker=self.ticker,
-                trade_type=position_type,
+                position_type=position_type,
                 quantity=quantity,
-                open_order_price=price,
+                observed_date=observed_date,
+                entry_submit_price=price,
                 action="SELL"
             )
-            self.position.place_open_order(price)
-            self.position_updated = True
-            self.db.open_order(self.position)
+            return quantity
+        self.submit_order(position_type=position_type, price=price)
 
-    def execute_order(self):
-        """Execute the current order using the broker."""
-        if self.position:
-            self.broker.execute_order(self.position)
+    def submit_order(self, position_type: str, price: float):
+        #Based on the position and order types, either update the database or create a new entry
+        action = self.position.action
+        position_type = self.position.position_type
+
+        if action == "BUY" and position_type == "LONG":
+            self.position.id = self.db.create_position(self.position)
+        elif action == "BUY" and position_type == "SHORT":
+            self.db.update_position(self.position)
+        elif action == "SELL" and position_type == "LONG":
+            self.position.id = self.db.create_position(self.position)
+        elif action == "SELL" and position_type == "SHORT":
+            self.db.update_position(self.position)
+
+        # execute the order
+        self.broker.execute_order(trade_position=self.position)    
+
--- broker.py
+++ broker.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 import pandas as pd
 from typing import Optional
 from .models import TradeKitPosition
@@ -6,35 +7,54 @@ from .database import TradeKitDB
 class TradeKitBroker:
     def __init__(self, db: TradeKitDB):
         self.db = db
+        self.position = None
 
-    def execute_order(self, position: TradeKitPosition, price: float):
+    def execute_order(self, trade_position: TradeKitPosition):
         """Execute the given order and update its status."""
-        if position.status == "PENDING":
-            if position.action == "BUY":
-                position.execute_open_order(price)
-                self.db.execute_open_trade(position, price, datetime.now())
-                print(f"Executed BUY order for {position.ticker} at {price}")
-            elif position.action == "SELL":
-                position.execute_close_order(price)
-                self.db.execute_close_trade(position, price, datetime.now())
-                print(f"Executed SELL order for {position.ticker} at {price}")
-        else:
-            print(f"Order for {position.ticker} is not in PENDING status")
 
-    def cancel_order(self, position: TradeKitPosition):
-        """Cancel the given order."""
-        if position.status == "PENDING":
-            position.cancel_order()
-            self.db.update_order_status(position.id, "CANCELED")
-            print(f"Canceled order for {position.ticker}")
-        else:
-            print(f"Order for {position.ticker} is not in PENDING status")
+        self.position = trade_position
+        if self.position.status == "PENDING":
+            ##################################################################
+            # submit the order to the broker API
+            ##################################################################
+            if (self.position.position_type == "LONG" and self.position.action == "BUY") or \
+               (self.position.position_type == "SHORT" and self.position.action == "SELL"):
+                self.position.status = "PENDING"
+                self.position.entry_submit_date = datetime.now()
+                self.position.entry_submit_price = self.position.entry_submit_price
+            elif (self.position.position_type == "SHORT" and self.position.action == "BUY") or \
+                 (self.position.position_type == "LONG" and self.position.action == "SELL"):
+                self.position.status = "PENDING"
+                self.position.exit_submit_date = datetime.now()
+                self.position.exit_submit_price = self.position.exit_submit_price
+            self.db.update_position(self.position)
 
-    def fail_order(self, position: TradeKitPosition):
-        """Mark the given order as failed."""
-        if position.status == "PENDING":
-            position.fail_order()
-            self.db.update_order_status(position.id, "FAILED")
-            print(f"Failed order for {position.ticker}")
+            ##################################################################
+            # execute the order throught the broker API
+            ##################################################################
+            if self.position.action == "BUY":
+                if self.position.position_type == "LONG":
+                    self.position.status = "OPEN"
+                    self.position.entry_date = datetime.now()
+                    self.position.entry_price = self.position.entry_submit_price
+                elif self.position.position_type == "SHORT":
+                    self.position.status = "CLOSED"
+                    self.position.exit_date = datetime.now()
+                    self.position.exit_price = self.position.exit_submit_price
+                self.db.update_position(self.position)
+                p = self.position.exit_submit_price or self.position.entry_submit_price
+                print(f"Executed BUY order for {self.position.ticker} at {p}")
+            elif self.position.action == "SELL":
+                if self.position.position_type == "LONG":
+                    self.position.status = "CLOSED"
+                    self.position.exit_date = datetime.now()
+                    self.position.exit_price = self.position.exit_submit_price  
+                elif self.position.position_type == "SHORT":
+                    self.position.status = "OPEN"
+                    self.position.entry_date = datetime.now()
+                    self.position.entry_price = self.position.entry_submit_price
+                self.db.update_position(self.position)
+                p = self.position.entry_submit_price or self.position.exit_submit_price
+                print(f"Executed SELL order for {self.position.ticker} at {p}")
         else:
-            print(f"Order for {position.ticker} is not in PENDING status")
+            print(f"Order for position id: {self.position.id} is not in PENDING status")
