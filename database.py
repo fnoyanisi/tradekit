9499eb15bba0680cc8c8424bb9578ac6be214a3b
Fehmi Noyan Isi <fnoyanisi@yahoo.com>
2025-03-23 00:24:24 +1300

    renamed the fields and reduced the num of methods

--- database.py
+++ database.py
@@ -36,17 +36,17 @@ class TradeKitDB:
             bot_name VARCHAR(50) NOT NULL,
             ticker VARCHAR(10) NOT NULL,
             observed_date TIMESTAMP NULL,
-            trade_type VARCHAR(5) CHECK (trade_type IN ('LONG', 'SHORT')) NOT NULL DEFAULT 'LONG',
+            position_type VARCHAR(5) CHECK (position_type IN ('LONG', 'SHORT')) NOT NULL DEFAULT 'LONG',
             quantity INT NOT NULL,
             action VARCHAR(4) CHECK (action IN ('BUY', 'SELL')) NOT NULL,
-            open_order_date TIMESTAMP NULL,
-            open_order_price NUMERIC(10,2) NOT NULL,
-            open_date TIMESTAMP NULL,
-            open_price NUMERIC(10, 2) NULL,
-            close_order_date TIMESTAMP NULL,
-            close_order_price NUMERIC(10,2) NULL,
-            close_date TIMESTAMP NULL,
-            close_price NUMERIC(10, 2) NULL,
+            entry_submit_date TIMESTAMP NULL,
+            entry_submit_price NUMERIC(10,2) NOT NULL,
+            entry_date TIMESTAMP NULL,
+            entry_price NUMERIC(10, 2) NULL,
+            exit_submit_date TIMESTAMP NULL,
+            exit_submit_price NUMERIC(10,2) NULL,
+            exit_date TIMESTAMP NULL,
+            exit_price NUMERIC(10, 2) NULL,
             order_type VARCHAR(6) CHECK (order_type IN ('LIMIT', 'MARKET')) NOT NULL DEFAULT 'MARKET',
             status VARCHAR(9) CHECK (status IN ('PENDING', 'OPEN', 'PARTIAL', 'CLOSED', 'CANCELED', 'FAILED')) NOT NULL
         );
@@ -55,71 +55,82 @@ class TradeKitDB:
             cur.execute(create_table_query)
             self.conn.commit()
 
-    def open_order(self, trade_position : TradeKitPosition):        
-        """Insert a new open trade order into the database."""
+    def update_position(self, trade_position: TradeKitPosition):
+        """ Generates the UPDATE SQL statement dynamically for an existing position. """
+        if trade_position.id is None:
+            raise ValueError("Position ID is required for updating the database.")
+
+        fields_to_update = {}
+        
+        # convert the passed object's attributes into a dictionary
+        position_data = vars(trade_position)
+
+        # create an UPDATE statement with only the fields that have values, i.e. not None
+        for field, value in position_data.items():
+            if value is not None and field != "id":
+                fields_to_update[field] = value
+
+        if not fields_to_update:
+            raise ValueError("No fields to update.")
+        
+        set_clause = ", ".join([f"{key} = %s" for key in fields_to_update.keys()])
+        values = list(fields_to_update.values())
+
+        sql_query = f"""
+        UPDATE trades
+        SET {set_clause}
+        WHERE id = %s
+        """
+
+        values.append(trade_position.id)
+        try:
+            cursor = self.conn.cursor()
+            cursor.execute(sql_query, values)
+            self.conn.commit()
+            print(f"Trade position {trade_position.id} updated successfully.")
+        except Exception as e:
+            print(f"Error updating trade position {trade_position.id}: {e}")
+
+    def create_position(self, trade_position: TradeKitPosition):        
+        """Insert a new open trade position into the database."""
+        
         insert_query = """
-        INSERT INTO trades (bot_name, ticker, trade_type, quantity, open_order_price, action, open_order_date, status)
-        VALUES (%s, %s, %s, %s, %s, %s, %s, 'PENDING')
+        INSERT INTO trades (bot_name, ticker, position_type, quantity, entry_submit_price, action, entry_submit_date, status)
+        VALUES (%(bot_name)s, %(ticker)s, %(position_type)s, %(quantity)s, %(entry_submit_price)s, %(action)s, %(entry_submit_date)s, %(status)s)
         RETURNING id;
         """
-        with self.conn.cursor() as cur:
-            cur.execute(insert_query, (trade_position.bot_name, 
-                                       trade_position.ticker, 
-                                       trade_position.trade_type,
-                                       trade_position.quantity, 
-                                       trade_position.open_order_price, 
-                                       trade_position.action, 
-                                       trade_position.open_order_date
-                                    )
-                        )
-            trade_id = cur.fetchone()['id']
-            self.conn.commit()
-        return trade_id
 
-    def close_order(self, trade_position: TradeKitPosition, close_order_price, close_order_date):
-        """Insert a new close trade order into the database."""
-        update_query = """
-        UPDATE trades
-        SET close_order_price = %s, close_order_date = %s, status = 'PENDING'
-        WHERE id = %s AND status = 'OPEN';
-        """
-        with self.conn.cursor() as cur:
-            cur.execute(update_query, (close_order_price, close_order_date, trade_position.id))
-            self.conn.commit()
-            print(f"Close order placed for trade {trade_position.id} successfully.")
+        params = {
+            "bot_name": trade_position.bot_name,
+            "ticker": trade_position.ticker,
+            "position_type": trade_position.position_type,
+            "quantity": trade_position.quantity,
+            "entry_submit_price": trade_position.entry_submit_price,
+            "action": trade_position.action,
+            "entry_submit_date": trade_position.entry_submit_date if trade_position.entry_submit_date else None,  # NULL handling
+            "status": trade_position.status if trade_position.status else "PENDING",  # default status is PENDING
+        }
 
-    def execute_open_trade(self, trade_position: TradeKitPosition, open_price, open_date):
-        """Update the trade with actual execution price and date for the open order."""
-        update_query = """
-        UPDATE trades
-        SET open_price = %s, open_date = %s, status = 'OPEN'
-        WHERE id = %s AND status = 'PENDING';
-        """
-        with self.conn.cursor() as cur:
-            cur.execute(update_query, (open_price, open_date, trade_position.id))
-            self.conn.commit()
-            print(f"Open trade executed for {trade_position.id}.")
-
-    def execute_close_trade(self, trade_position: TradeKitPosition, close_price, close_date):
-        """Update the trade with actual execution price and date for the close order."""
-        update_query = """
-        UPDATE trades
-        SET close_price = %s, close_date = %s, status = 'CLOSED'
-        WHERE id = %s AND status = 'PENDING';
-        """
-        with self.conn.cursor() as cur:
-            cur.execute(update_query, (close_price, close_date, trade_position.id))
-            self.conn.commit()
-            print(f"Close trade executed for {trade_position.id}.")
+        try:
+            with self.conn.cursor() as cur:
+                cur.execute(insert_query, params)
+                trade_id = cur.fetchone()["id"]
+                self.conn.commit()
+                return trade_id
+        
+        except Exception as e:
+            self.conn.rollback()  # Ensure rollback on failure
+            print(f"Error creating trade position: {e}")
+            return None
 
     def get_latest_open_position(self, bot_name, ticker):
         """Fetch the latest open trade position for a bot on a specific ticker."""
         query = """
-        SELECT id, bot_name, ticker, trade_type, quantity, action, open_order_date, open_order_price, 
-            open_date, open_price, close_order_date, close_order_price, close_date, close_price, status
+        SELECT id, bot_name, ticker, position_type, quantity, action, entry_submit_date, entry_submit_price, 
+            entry_date, entry_price, exit_submit_date, exit_submit_price, exit_date, exit_price, status
         FROM trades
         WHERE bot_name = %s AND ticker = %s AND status = 'OPEN'
-        ORDER BY open_date DESC
+        ORDER BY entry_date DESC
         LIMIT 1;
         """
         with self.conn.cursor() as cur:
@@ -131,17 +142,17 @@ class TradeKitDB:
                     id=row["id"],
                     bot_name=row["bot_name"],
                     ticker=row["ticker"],
-                    trade_type=row['trade_type'],
+                    position_type=row['position_type'],
                     quantity=row["quantity"],
                     action=row["action"],
-                    open_order_price=row["open_order_price"],
-                    open_order_date=row["open_order_date"],
-                    open_date=row["open_date"],
-                    open_price=row["open_price"],
-                    close_order_date=row["close_order_date"],
-                    close_order_price=row["close_order_price"],
-                    close_date=row["close_date"],
-                    close_price=row["close_price"],
+                    entry_submit_price=row["entry_submit_price"],
+                    entry_submit_date=row["entry_submit_date"],
+                    entry_date=row["entry_date"],
+                    entry_price=row["entry_price"],
+                    exit_submit_date=row["exit_submit_date"],
+                    exit_submit_price=row["exit_submit_price"],
+                    exit_date=row["exit_date"],
+                    exit_price=row["exit_price"],
                     status=row["status"]
                 )
             else:
@@ -150,11 +161,11 @@ class TradeKitDB:
     def get_last_position(self, bot_name, ticker):
         """Fetch the latest open trade position for a bot on a specific ticker."""
         query = """
-        SELECT id, bot_name, ticker, trade_type, quantity, action, open_order_date, open_order_price, 
-            open_date, open_price, close_order_date, close_order_price, close_date, close_price, status
+        SELECT id, bot_name, ticker, position_type, quantity, action, entry_submit_date, entry_submit_price, 
+            entry_date, entry_price, exit_submit_date, exit_submit_price, exit_date, exit_price, status
         FROM trades
         WHERE bot_name = %s AND ticker = %s AND status != 'CLOSED'
-        ORDER BY open_date DESC
+        ORDER BY entry_date DESC
         LIMIT 1;
         """
         with self.conn.cursor() as cur:
@@ -166,41 +177,21 @@ class TradeKitDB:
                     id=row["id"],
                     bot_name=row["bot_name"],
                     ticker=row["ticker"],
-                    trade_type=row['trade_type'],
+                    position_type=row['position_type'],
                     quantity=row["quantity"],
                     action=row["action"],
-                    open_order_price=row["open_order_price"],
-                    open_order_date=row["open_order_date"],
-                    open_date=row["open_date"],
-                    open_price=row["open_price"],
-                    close_order_date=row["close_order_date"],
-                    close_order_price=row["close_order_price"],
-                    close_date=row["close_date"],
-                    close_price=row["close_price"],
+                    entry_submit_price=row["entry_submit_price"],
+                    entry_submit_date=row["entry_submit_date"],
+                    entry_date=row["entry_date"],
+                    entry_price=row["entry_price"],
+                    exit_submit_date=row["exit_submit_date"],
+                    exit_submit_price=row["exit_submit_price"],
+                    exit_date=row["exit_date"],
+                    exit_price=row["exit_price"],
                     status=row["status"]
                 )
             else:
                 return None
-
-    def get_all_trades(self):
-        """Retrieve all trades from the database."""
-        with self.conn.cursor() as cur:
-            cur.execute("SELECT * FROM trades;")
-            return cur.fetchall()
-
-    def get_trades_by_bot(self, bot_name):
-        """Retrieve trades made by a specific bot."""
-        query = "SELECT * FROM trades WHERE bot_name = %s;"
-        with self.conn.cursor() as cur:
-            cur.execute(query, (bot_name,))
-            return cur.fetchall()
-
-    def get_trades_by_ticker(self, ticker):
-        """Retrieve trades for a specific ticker."""
-        query = "SELECT * FROM trades WHERE ticker = %s;"
-        with self.conn.cursor() as cur:
-            cur.execute(query, (ticker,))
-            return cur.fetchall()
 
     def close(self):
         """Close database connection."""
