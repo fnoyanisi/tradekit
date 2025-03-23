f45374a13dad600bae409650ac1dc2bc4a052dbb
Fehmi Noyan Isi <fnoyanisi@yahoo.com>
2025-03-23 14:57:18 +1300

    sell_aggressiveness and quantity for sell()

--- bot.py
+++ bot.py
@@ -14,24 +14,47 @@ class TradeKitBot:
         self.db = db
         self.position = None
         self.cash = 0.0
+        self.position_size = 0
 
         # determines the % of cash that can be spent 
         # on a single trade. Default is "moderate"
-        self.aggressiveness_levels = {
+        self.buy_aggressiveness_levels = {
             "max": 1.0,         # 100%
             "moderate": 0.6,    # 60%
             "conservative": 0.4 # 40%
         }
-        self.aggressiveness = "moderate"
+        self.buy_aggressiveness = "moderate"
 
-    def set_aggressiveness(self, level: str):
-        if level in self.aggressiveness_levels:
-            self.aggressiveness = level
+        # determines the % of assets that can be sold 
+        # on a single trade. Default is "max"
+        self.sell_aggressiveness_levels = {
+            "max": 1.0,         # 100%
+            "moderate": 0.6,    # 60%
+            "conservative": 0.4 # 40%
+        }
+        self.sell_aggressiveness = "max"
+
+    def set_buy_aggressiveness(self, level: str):
+        if level in self.buy_aggressiveness_levels:
+            self.buy_aggressiveness = level
         else:
             raise ValueError(f"Invalid aggressiveness level: {level}")
     
-    def set_aggressiveness_ratios(self, max_ratio: float, moderate_ratio: float, conservative_ratio: float):
-        self.aggressiveness_levels = {
+    def set_buy_aggressiveness_ratios(self, max_ratio: float, moderate_ratio: float, conservative_ratio: float):
+        self.buy_aggressiveness_levels = {
+            "max": max_ratio,
+            "moderate": moderate_ratio,
+            "conservative": conservative_ratio
+        }
+
+    def set_sell_aggressiveness(self, level: str):
+        if level in self.sell_aggressiveness_levels:
+            self.sell_aggressiveness = level
+        else:
+            raise ValueError(f"Invalid aggressiveness level: {level}")
+    
+    def set_sell_aggressiveness_ratios(self, max_ratio: float, moderate_ratio: float, conservative_ratio: float):
+        self.sell_aggressiveness_levels = {
             "max": max_ratio,
             "moderate": moderate_ratio,
             "conservative": conservative_ratio
@@ -79,17 +102,23 @@ class TradeKitBot:
             self.load_position()
             self.strategy(self)
 
-    def calculate_quantity(self, price: float) -> int:
+    def calculate_buy_quantity(self, price: float) -> int:
         """Determine how many shares to buy based on available cash and aggressiveness level."""
-        ratio = self.aggressiveness_levels[self.aggressiveness]
+        ratio = self.buy_aggressiveness_levels[self.buy_aggressiveness]
         budget = self.cash * ratio
         budget_after_commission = budget * (1 - self.commission)
         quantity = int(budget_after_commission / price)
         return max(quantity, 0)
+    
+    def calculate_sell_quantity(self) -> int:
+        """Determine how many shares to sell based on position size and sell aggressiveness level."""
+        ratio = self.sell_aggressiveness_levels[self.sell_aggressiveness]
+        quantity = int(self.position_size * ratio)
+        return max(quantity, 0)
 
     def buy(self, position_type: str, price: float, observed_date: Optional[str] = None):
         """Place a buy order."""
-        quantity = self.calculate_quantity(price)
+        quantity = self.calculate_buy_quantity(price)
         if quantity > 0:
             if not self.position:
                 self.position = TradeKitPosition(
@@ -108,20 +137,26 @@ class TradeKitBot:
             print("Not enough cash to place a trade: " + str(self.cash))
             return -1
 
-    def sell(self, position_type:str, quantity: int, price: float, observed_date: Optional[str] = None):
+    def sell(self, position_type: str, price: float, observed_date: Optional[str] = None):
         """Place a sell order."""
-        if not self.position:
-            self.position = TradeKitPosition(
-                bot_name=self.name,
-                ticker=self.ticker,
-                position_type=position_type,
-                quantity=quantity,
-                observed_date=observed_date,
-                entry_submit_price=price,
-                action="SELL"
-            )
+        quantity = self.calculate_sell_quantity()
+        if quantity > 0:
+            if not self.position:
+                self.position = TradeKitPosition(
+                    bot_name=self.name,
+                    ticker=self.ticker,
+                    position_type=position_type,
+                    quantity=quantity,
+                    observed_date=observed_date,
+                    entry_submit_price=price,
+                    action="SELL"
+                )
+            # The order hasn't been executed yet
+            self.submit_order(position_type=position_type, price=price)
             return quantity
-        self.submit_order(position_type=position_type, price=price)
+        else:
+            print("Not enough shares to sell: " + str(self.position_size))
+            return -1
 
     def submit_order(self, position_type: str, price: float):
         #Based on the position and order types, either update the database or create a new entry
