257aec91e6124343ce10afd8b607870e78d606bd
Fehmi Noyan Isi <fnoyanisi@yahoo.com>
2025-03-25 00:27:46 +1300

    fixed bugs

--- broker.py
+++ broker.py
@@ -11,50 +11,61 @@ class TradeKitBroker:
 
     def execute_order(self, trade_position: TradeKitPosition):
         """Execute the given order and update its status."""
-
         self.position = trade_position
-        if self.position.status == "PENDING":
+        if self.position.status == "PENDING" or self.position.status == "OPEN":
             ##################################################################
-            # submit the order to the broker API
+            # Submit the order to the broker API
             ##################################################################
             if (self.position.position_type == "LONG" and self.position.action == "BUY") or \
-               (self.position.position_type == "SHORT" and self.position.action == "SELL"):
+            (self.position.position_type == "SHORT" and self.position.action == "SELL"):
                 self.position.status = "PENDING"
                 self.position.entry_submit_date = datetime.now()
                 self.position.entry_submit_price = self.position.entry_submit_price
             elif (self.position.position_type == "SHORT" and self.position.action == "BUY") or \
-                 (self.position.position_type == "LONG" and self.position.action == "SELL"):
+                (self.position.position_type == "LONG" and self.position.action == "SELL"):
                 self.position.status = "PENDING"
                 self.position.exit_submit_date = datetime.now()
                 self.position.exit_submit_price = self.position.exit_submit_price
             self.db.update_position(self.position)
 
             ##################################################################
-            # execute the order throught the broker API
+            # Execute the order through the broker API
             ##################################################################
             if self.position.action == "BUY":
                 if self.position.position_type == "LONG":
                     self.position.status = "OPEN"
                     self.position.entry_date = datetime.now()
                     self.position.entry_price = self.position.entry_submit_price
+                    self.db.update_position(self.position)
+                    p = self.position.entry_submit_price
+                    print(f"{self.__class__.__name__} : Executed BUY order for {self.position.ticker} at {p}")
+                    return self.position.position_size  # Return the position_size bought
                 elif self.position.position_type == "SHORT":
                     self.position.status = "CLOSED"
                     self.position.exit_date = datetime.now()
                     self.position.exit_price = self.position.exit_submit_price
-                self.db.update_position(self.position)
-                p = self.position.exit_submit_price or self.position.entry_submit_price
-                print(f"{self.__class__.__name__} : Executed BUY order for {self.position.ticker} at {p}")
+                    self.db.update_position(self.position)
+                    p = self.position.exit_submit_price
+                    print(f"{self.__class__.__name__} : Executed BUY order for {self.position.ticker} at {p}")
+                    return self.position.position_size  # Return the position_size bought
             elif self.position.action == "SELL":
                 if self.position.position_type == "LONG":
                     self.position.status = "CLOSED"
                     self.position.exit_date = datetime.now()
-                    self.position.exit_price = self.position.exit_submit_price  
+                    self.position.exit_price = self.position.exit_submit_price
+                    self.db.update_position(self.position)
+                    p = self.position.exit_submit_price
+                    print(f"{self.__class__.__name__} : Executed SELL order for {self.position.ticker} at {p}")
+                    return self.position.position_size  # Return the position_size sold
                 elif self.position.position_type == "SHORT":
                     self.position.status = "OPEN"
                     self.position.entry_date = datetime.now()
                     self.position.entry_price = self.position.entry_submit_price
-                self.db.update_position(self.position)
-                p = self.position.entry_submit_price or self.position.exit_submit_price
-                print(f"{self.__class__.__name__} : Executed SELL order for {self.position.ticker} at {p}")
+                    self.db.update_position(self.position)
+                    p = self.position.entry_submit_price
+                    print(f"{self.__class__.__name__} : Executed SELL order for {self.position.ticker} at {p}")
+                    return self.position.position_size  # Return the position_size sold
         else:
-            print(f"Order for position id: {self.position.id} is not in PENDING status")
+            print(f"{self.__class__.__name__} : Order for position id: {self.position.id} is not in PENDING status")
+            return -1
+
